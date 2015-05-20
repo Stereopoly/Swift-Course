30ced0faa91f056a7a680c3223e84f8f88addda8
Stereopoly <Oscar@Oscars-iMac.local>
2015-05-19 20:17:03 -0700

    Frameworks

--- Tinder Clone/Tinder Clone/AppDelegate.swift
+++ Tinder Clone/Tinder Clone/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.setApplicationId("WwAUDlXEtJbMbpIS67cDi1FhlfV3rSz3EY87An6r", clientKey:"c1A27ErDaYrYNHZyRUxhyorDdMskqMcVp9ZqCcz5")
+        
+        PFFacebookUtils.initializeFacebook()
+        
         return true
     }
 
@@ -39,6 +44,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(application: UIApplication,
+        openURL url: NSURL,
+        sourceApplication: String?,
+        annotation: AnyObject?) -> Bool {
+            return FBSDKApplicationDelegate.sharedInstance.application(application,
+                openURL: url,
+                sourceApplication: sourceApplication,
+                annotation: annotation)
+    }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        FBSDKAppEvents.activateApp()
     }
 
 
--- Tinder Clone/Tinder Clone/ViewController.swift
+++ Tinder Clone/Tinder Clone/ViewController.swift
@@ -13,6 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var permissions = ["public_profile"]
+        
+        PFFacebookUtils.logInInBackgroundWithReadPermissions(permissions, {
+            (user: PFUser?, error: NSError?) -> Void in
+            if user != nil {
+                println("User signed up and logged in through Facebook!")
+            else if user?.isNew{
+                    println("User logged in through Facebook!")
+            }
+            } else {
+                println("Uh oh. The user cancelled the Facebook login.")
+            }
+        })
+        
     }
 
     override func didReceiveMemoryWarning() {
